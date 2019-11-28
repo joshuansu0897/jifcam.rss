db119798d41a0a42731f070aa580e0db9e30198a
Joshua Saucedo <joshuansu0897@gmail.com>
2019-11-28 11:47:04 -0700

    terminate without imit

--- index.js
+++ index.js
@@ -2,33 +2,28 @@ const { Worker } = require('worker_threads')
 const csv = require('fast-csv')
 const fs = require('fs')
 
-let workersLimit = 0
-
 const runService = async (workerData) => {
-  return new Promise(resolve => {
-    const worker = new Worker('./src/utils/worker.js', { workerData })
+  const worker = new Worker('./src/utils/worker.js', { workerData })
 
-    worker.on('message', (incoming) => {
-      console.log({ incoming })
-      if (incoming === 'err!') {
-        terminateWorker(worker)
-      }
-    })
-
-    worker.on('error', (code) => {
-      new Error(`Worker error with exit code ${code}`)
+  worker.on('message', (incoming) => {
+    console.log({ incoming })
+    if (incoming === 'err!') {
       terminateWorker(worker)
-    })
-
-    worker.on('exit', (code) =>
-      console.log(`Worker stopped with exit code ${code}`)
-    )
+    }
   })
+
+  worker.on('error', (code) => {
+    new Error(`Worker error with exit code ${code}`)
+    terminateWorker(worker)
+  })
+
+  worker.on('exit', (code) =>
+    console.log(`Worker stopped with exit code ${code}`)
+  )
 }
 
 function terminateWorker(worker) {
   worker.terminate()
-  workersLimit--
   console.log('TERMINATE!')
 }
 
@@ -38,14 +33,11 @@ async function run() {
   fs.createReadStream('./rss-links-large.csv')
     .pipe(csv.parse({ headers: true }))
     .on('data', (row) => {
-      if (row.RSS === null || row.RSS === undefined || row.RSS === 'No RSS' || workersLimit === 1000) {
+      if (row.RSS === null || row.RSS === undefined || row.RSS === 'No RSS') {
         return
       }
       listOfPromises.push(runService(row.RSS))
-      workersLimit++
     })
-
-  return await Promise.all(listOfPromises)
 }
 
 run().catch(err => console.error(err))
