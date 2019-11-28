a1193b7355293fd797ecdf9e897dca9af35a531e
Joshua Saucedo <joshuansu0897@gmail.com>
2019-11-28 11:36:28 -0700

    terminate and limit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Worker } = require('worker_threads')
 const csv = require('fast-csv')
 const fs = require('fs')
 
+let workersLimit = 0
+
 const runService = async (workerData) => {
   return new Promise(resolve => {
     const worker = new Worker('./src/utils/worker.js', { workerData })
@@ -9,13 +11,13 @@ const runService = async (workerData) => {
     worker.on('message', (incoming) => {
       console.log({ incoming })
       if (incoming === 'err!') {
-        worker.terminate()
+        terminateWorker(worker)
       }
     })
 
     worker.on('error', (code) => {
       new Error(`Worker error with exit code ${code}`)
-      worker.terminate()
+      terminateWorker(worker)
     })
 
     worker.on('exit', (code) =>
@@ -24,16 +26,23 @@ const runService = async (workerData) => {
   })
 }
 
+function terminateWorker(worker) {
+  worker.terminate()
+  workersLimit--
+  console.log('TERMINATE!')
+}
+
 async function run() {
   const listOfPromises = []
 
   fs.createReadStream('./rss-links-large.csv')
     .pipe(csv.parse({ headers: true }))
     .on('data', (row) => {
-      if (row.RSS === null || row.RSS === undefined || row.RSS === 'No RSS') {
+      if (row.RSS === null || row.RSS === undefined || row.RSS === 'No RSS' || workersLimit === 1000) {
         return
       }
       listOfPromises.push(runService(row.RSS))
+      workersLimit++
     })
 
   return await Promise.all(listOfPromises)
